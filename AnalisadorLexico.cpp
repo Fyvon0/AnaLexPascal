5b66887b65e9225c56ce9362b9e385066292bd07
Felipe Martins <thefemr@gmail.com>
2017-11-12 08:01:34 -0200

    Correção da iteração

--- AnalisadorLexico.cpp
+++ AnalisadorLexico.cpp
@@ -22,9 +22,31 @@ AnalisadorLexico::AnalisadorLexico(string nomeArq)
     strstream << this->arq.rdbuf();
     tmp = strstream.str();
 
-    for(regex_iterator<string> i = regex_iterator<string>(tmp.begin(), tmp.end(), AnalisadorLexico::regex_pattern); i != regex_iterator<string>(); ++i ) {
+    /*for(regex_iterator<string> i = regex_iterator<string>(tmp.begin(), tmp.end(), AnalisadorLexico::regex_pattern); i != regex_iterator<string>(); ++i ) {
         smatch match = *i;
         cout << match.str();
+    }*/
+
+    try {
+        //regex re("\\w+");
+        sregex_iterator seguinte(tmp.begin(), tmp.end(), AnalisadorLexico::regex_pattern);
+        sregex_iterator fim;
+        while (seguinte != fim) {
+            smatch match = *seguinte;
+            cout << "0 - " << string(match[0]) << "\n";
+            cout << "1 - " << string(match[1]) << "\n";
+            cout << "2 - " << string(match[2]) << "\n";
+            cout << "3 - " << string(match[3]) << "\n";
+            cout << "4 - " << string(match[4]) << "\n";
+            cout << "5 - " << string(match[5]) << "\n";
+            cout << "6 - " << string(match[6]) << "\n";
+            cout << match.str() << "\n";
+            seguinte++;
+        }
+    } catch (regex_error& e) {
+        // Syntax error in the regular expression
+    } catch (...) {
+        //Vai saber
     }
 }
 
